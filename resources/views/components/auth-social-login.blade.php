cc5db637615626aafa57de055d2316b89f342402
Univan0928 <univan0928@gmail.com>
2022-06-23 23:11:02 +0800

    some updates

--- resources/views/components/auth-social-login.blade.php
+++ resources/views/components/auth-social-login.blade.php
@@ -1,6 +1,6 @@
 @if(env('FACEBOOK_ACTIVE') || env('GITHUB_ACTIVE') || env('GOOGLE_ACTIVE'))
 <div class="mb-4">
-    <div class="text-center mt-2 mb-4">
+    <!-- <div class="text-center mt-2 mb-4">
         Sign in with social profiles
     </div>
 
@@ -24,6 +24,6 @@
         @endif
     </div>
 
-    <hr>
+    <hr> -->
 </div>
 @endif
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -16,10 +16,10 @@
             </h1>
             <p class="mb-8 sm:text-4xl text-3xl">
                 <!-- {!! setting('meta_description') !!} -->
-                User and Role management Application Backend, Backup, Logviewer are available here.
+                <!-- User and Role management Application Backend, Backup, Logviewer are available here. -->
             </p>
             <div class="flex justify-center">
-                <a href="https://github.com/nasirkhan" class="inline-flex text-white bg-gray-700 border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 rounded text-lg">
+                <a href="https://pin.ski/3Hswsp4" target="_blank" class="inline-flex text-white bg-gray-700 border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 rounded text-lg">
                 <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clip-path="url(#clip0_435_2669)">
                 <path d="M7.40099 15.5039L7.46399 15.4019L11.367 9.29993C11.3803 9.28017 11.3987 9.26429 11.4201 9.25392C11.4416 9.24355 11.4654 9.23905 11.4892 9.24088C11.513 9.24271 11.5359 9.2508 11.5555 9.26433C11.5751 9.27787 11.5908 9.29636 11.601 9.31793C12.252 10.7789 12.801 12.5969 12.552 13.7279C12.3735 14.3193 12.1151 14.8835 11.784 15.4049C11.739 15.4889 11.691 15.5729 11.637 15.6539C11.6246 15.6718 11.608 15.6864 11.5886 15.6964C11.5693 15.7063 11.5478 15.7113 11.526 15.7109H7.51499C7.49046 15.7119 7.46613 15.7061 7.44475 15.694C7.42336 15.682 7.40575 15.6642 7.39391 15.6427C7.38207 15.6212 7.37645 15.5968 7.3777 15.5723C7.37896 15.5478 7.38702 15.5241 7.40099 15.5039Z" fill="white"/>
@@ -33,7 +33,7 @@
                 </svg>
                     MVP(NFT)
                 </a>
-                <a href="https://ownhawaii.com/" class="ml-4 outline outline-1 outline-gray-800 inline-flex text-gray-700 hover:text-gray-100 bg-gray-200 hover:bg-gray-700 py-2 px-6 focus:outline-none rounded text-lg">
+                <a href="https://ownhawaii.com/" target="_blank" class="ml-4 outline outline-1 outline-gray-800 inline-flex text-gray-700 hover:text-gray-100 bg-gray-200 hover:bg-gray-700 py-2 px-6 focus:outline-none rounded text-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="w-8 h-8" viewBox="0 0 18 18">
                         <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855-.143.268-.276.56-.395.872.705.157 1.472.257 2.282.287V1.077zM4.249 3.539c.142-.384.304-.744.481-1.078a6.7 6.7 0 0 1 .597-.933A7.01 7.01 0 0 0 3.051 3.05c.362.184.763.349 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9.124 9.124 0 0 1-1.565-.667A6.964 6.964 0 0 0 1.018 7.5h2.49zm1.4-2.741a12.344 12.344 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332zM8.5 5.09V7.5h2.99a12.342 12.342 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.612 13.612 0 0 1 7.5 10.91V8.5H4.51zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741H8.5zm-3.282 3.696c.12.312.252.604.395.872.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a6.696 6.696 0 0 1-.598-.933 8.853 8.853 0 0 1-.481-1.079 8.38 8.38 0 0 0-1.198.49 7.01 7.01 0 0 0 2.276 1.522zm-1.383-2.964A13.36 13.36 0 0 1 3.508 8.5h-2.49a6.963 6.963 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667zm6.728 2.964a7.009 7.009 0 0 0 2.275-1.521 8.376 8.376 0 0 0-1.197-.49 8.853 8.853 0 0 1-.481 1.078 6.688 6.688 0 0 1-.597.933zM8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855.143-.268.276-.56.395-.872A12.63 12.63 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.963 6.963 0 0 0 14.982 8.5h-2.49a13.36 13.36 0 0 1-.437 3.008zM14.982 7.5a6.963 6.963 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008h2.49zM11.27 2.461c.177.334.339.694.482 1.078a8.368 8.368 0 0 0 1.196-.49 7.01 7.01 0 0 0-2.275-1.52c.218.283.418.597.597.932zm-.488 1.343a7.765 7.765 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z" />
                     </svg>
@@ -50,13 +50,14 @@
 <section class="">
     <div class="container mx-auto flex px-5 py-10 sm:py-24 items-center justify-center flex-col">
         <div class="text-center lg:w-2/3 w-full">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800">
+            <h1 class="title-font sm:text-4xl text-3xl font-medium text-gray-800">
             Quick overview of using the admin panel
             </h1>
             <p class="mb-8 leading-relaxed">
-            <div>Admin panel has five roles: Super Admin, Admin Istrator, Manager, Executive User, General User.</div>
-            
-            <div>General user who has just registered will not have direct access to the admin panel.</div>
+            <div>This admin panel was originally developed for obtaining and managing home addresses in Hawaii, but has designed and developed the infrastructure to extend to other cities around the world.</div>    
+            <div>Admin panel has five levels: Super Admin, Admin Istrator, Manager, Executive User, General User.</div>            
+            <div>A general user who has just registered will not have direct access to the admin panel.</div>
+            <div>Only Super Admin can grant the access to admin panel and give some roles to a new general user.</div>
             </p>
         </div>
     </div>
